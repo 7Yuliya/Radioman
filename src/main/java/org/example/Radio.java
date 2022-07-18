85ae0b9a37f2ea17bc9f0760606583fb725971c4
Юлия Алексина <uliyaalexina@mail.ru>
2022-07-18 06:54:43 -0700

    Added src/

---
+++ src/main/java/org/example/Radio.java
@@ -0,0 +1,75 @@
+package org.example;
+
+public class Radio {
+    public int currentRadioNumber;
+    public int maxCurrentRadioNumber = 9;
+    public int minCurrentRadioNumber = 0;
+
+    public int getCurrentRadioNumber() {
+        return currentRadioNumber;
+    }
+
+    public void setCurrentRadioNumber(int newCurrentRadioNumber) {
+        if (newCurrentRadioNumber < 0) {
+            return;
+        }
+        if (newCurrentRadioNumber > 9) {
+            return;
+        }
+        currentRadioNumber = newCurrentRadioNumber;
+
+    }
+
+    public void nextCurrentRadioNumber() {
+        if (currentRadioNumber < 9) {
+            currentRadioNumber = currentRadioNumber + 1;
+        }
+
+        if (currentRadioNumber == 9) {
+            currentRadioNumber = 0;
+        }
+
+    }
+
+    public void prevCurrentRadioNumber() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        if (currentRadioNumber == 0) {
+            currentRadioNumber = 9;
+        }
+    }
+
+    public int currentVolume;
+    public int maxCurrentVolume = 10;
+    public int minCurrentVolume = 0;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
+
+
---
+++ src/test/java/org/example/RadioTest.java
@@ -0,0 +1,63 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void shouldSetNextCurrentRadioNumber() {
+        Radio cond = new Radio();
+        cond.nextCurrentRadioNumber();
+        int expected = 1;
+        int actual = cond.getCurrentRadioNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldSetPrevCurrentRadioNumber() {
+        Radio cond = new Radio();
+        cond.prevCurrentRadioNumber();
+        int expected = 9;
+        int actual = cond.getCurrentRadioNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetIncreaseVolume() {
+        Radio cond = new Radio();
+        cond.increaseVolume();
+        int expected = 1;
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetDecreaseVolume() {
+        Radio cond = new Radio();
+        cond.decreaseVolume();
+        int expected = 0;
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetCurrentRadioNumber() {
+        Radio cond = new Radio();
+        cond.setCurrentRadioNumber(9);
+        int expected = 9;
+        int actual = cond.getCurrentRadioNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetCurrentVolume() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(10);
+        int expected = 10;
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+}
